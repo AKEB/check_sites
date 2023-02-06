24e453f398296eb6669b37b61a6667472aae5994
Vadim Babajanyan <babadzhanyan@corp.mail.ru>
2023-02-06 11:52:41 +0300

    first commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pyinstaller -D -F --clean -y -n check_sites main.py
---
+++ main.py
@@ -0,0 +1,66 @@
+import requests
+import time
+import signal
+import sys
+import hashlib
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+sites = [
+    'https://akeb.ru/',
+    'https://ats.my.games/',
+]
+
+caches = {}
+
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = '309690176:AAG3eLnw2uLUyojkwE_fnj9xRZjDxzJnQuQ'
+    bot_chatID = '160968329'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+
+
+def signal_handler(signal, frame):
+    print("\nYou pressed Ctrl+C!\n")
+    sys.exit(0)
+
+
+def check_result(site_uri: str, status: bool):
+    key = hashlib.md5(site_uri.encode('utf-8')).hexdigest()
+    cache = caches[key] if key in caches else {'status': None, 'time': 0}
+    if ('status' not in cache or cache['status'] != status) or (not status and cache['time'] < time.time() - 300):
+        if not status:
+            message = "🚫 " + site_uri + " не доступен!"
+        else:
+            message = "✅ " + site_uri + " доступен!"
+        telegram_bot_sendtext(message)
+        cache['time'] = time.time()
+    cache['status'] = status
+    caches[key] = cache
+
+
+def check_site(site_uri: str):
+    # print("Check " + site_uri)
+    try:
+        headers = {
+            'Accept-language': 'en',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15'
+        }
+        response = requests.get(site_uri, verify=False, headers=headers, timeout=(3, 6))
+        if not response or response.status_code not in [200, 201, 301, 302]:
+            check_result(site_uri, False)
+        else:
+            check_result(site_uri, True)
+    except requests.exceptions.Timeout as e:
+        check_result(site_uri, False)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+while True:
+    for site_uri in sites:
+        check_site(site_uri)
+    time.sleep(5)
